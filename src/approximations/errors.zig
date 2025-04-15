90cd8c09f1d0d021e0b99abb5fc4f6103daa0cec
Luca <drwolf85@gmail.com>
2025-04-15 18:54:21 -0400

    Generalized data type for computing approximations

--- src/approximations/errors.zig
+++ src/approximations/errors.zig
@@ -1,27 +1,22 @@
 const std = @import("std");
 const print = std.debug.print;
 
-export fn absolute_error(approx: f64, truth: f64) f64 {
+pub fn absolute_error(comptime T: type, approx: T, truth: T) T {
     return approx - truth;
 }
 
-export fn relative_error(approx: f64, truth: f64) f64 {
-    const err: f64 = approx - truth;
+pub fn relative_error(comptime T: type, approx: T, truth: T) T {
+    const err: T = approx - truth;
     return err / truth;
 }
 
-export fn approx_value(truth: f64, rel_err: f64) f64 {
+pub fn approx_value(comptime T: type, truth: T, rel_err: T) T {
     return truth * (1.0 + rel_err);
 }
 
-
-
 test "\nBasic error functions" {
-    print("\nAbs. err. (3 vs 2): {}\n",
-              .{absolute_error(3.0, 2.0)});
-    print("Rel. err. (3 vs 2): {}\n",
-              .{relative_error(3.0, 2.0)});
-    print("Approx. val. (2.0, 1%): {}\n", 
-              .{approx_value(2.0, 0.01)});
+    const mft: type = f64;
+    print("\nAbs. err. (3 vs 2): {}\n", .{absolute_error(mft, 3.0, 2.0)});
+    print("Rel. err. (3 vs 2): {}\n", .{relative_error(mft, 3.0, 2.0)});
+    print("Approx. val. (2.0, 1%): {}\n", .{approx_value(mft, 2.0, 0.01)});
 }
-
--- src/approximations/first_deriv.zig
+++ src/approximations/first_deriv.zig
@@ -1,20 +1,19 @@
 const std = @import("std");
 const print = std.debug.print;
 
-pub export fn deriv1(x0: f64, h: f64, htype: f64, f: ?*const fn (f64) callconv(.C) f64) f64 {
-    var res: f64 = 0.0;
-    var xl: f64 = x0;
-    var xr: f64 = x0;    
+pub fn deriv1(comptime T: type, x0: T, h: T, htype: T, f: ?*const fn (f64) callconv(.C) f64) T {
+    var xl: T = x0;
+    var xr: T = x0;
     xl += h * (1.0 - htype);
     xr -= h * htype;
-    res = f.?(xl) - f.?(xr);
-    return res / h;
+    const d64: f64 = f.?(@as(f64, @floatCast(xl))) - f.?(@as(f64, @floatCast(xr)));
+    return @as(T, @floatCast(d64)) / h;
 }
 
 pub extern fn exp(__x: f64) f64;
 
 test "Testing first derivatives" {
-    print("\nFirst derivative of exp(x) in x = 1: {}\n",
-              .{deriv1(1.0, 0.000000000001, 0.5, &exp)});
+    const mft: type = f64;
+    print("\nTrue first derivative of exp(x) in x = 1: {}", .{@exp(@as(mft, 1.0))});
+    print("\nFirst derivative of exp(x) in x = 1: {}\n", .{deriv1(mft, 1.0, 0.0000005, 0.5, &exp)});
 }
-
