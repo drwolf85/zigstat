0c90f1e29e4e93e45492841e9800aa9fa70dbe81
Luca <drwolf85@gmail.com>
2025-04-15 18:22:10 -0400

    Generalized data types for median computations

--- src/descriptive_stat/quantiles/median.zig
+++ src/descriptive_stat/quantiles/median.zig
@@ -1,22 +1,23 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn median(a: [*]f64, len: usize) f64 {
-        var res : f64 = undefined;
-        const x = a[0..len];
-        const pos : usize = len >> 1;
-        std.mem.sort(f64, x, {}, std.sort.asc(f64));
-        res = x[pos];
-        if (len > 1 and (len & 1) == 0) {
-                res += x[pos-1];
-                res *= 0.5;
-        }
-        return res;
+pub fn median(comptime T: type, a: [*]T, len: usize) T {
+    var res: T = undefined;
+    const x = a[0..len];
+    const pos: usize = len >> 1;
+    std.mem.sort(T, x, {}, std.sort.asc(T));
+    res = x[pos];
+    if (len > 1 and (len & 1) == 0) {
+        res += x[pos - 1];
+        res *= 0.5;
+    }
+    return res;
 }
 
 test "Median function" {
-        var a = [_]f64 {0.1, -0.1, -0.2, 1.5, 1.7};
-        var b = [_]f64 {-0.1, -0.2, 1.5, 1.7};
-        std.debug.print("Median: {}\n", .{median(&a, a.len)});
-        std.debug.print("Median: {}\n", .{median(&b, b.len)});
+    const mft: type = f64;
+    var a = [_]mft{ 0.1, -0.1, -0.2, 1.5, 1.7 };
+    var b = [_]mft{ -0.1, -0.2, 1.5, 1.7 };
+    std.debug.print("Median: {}\n", .{median(mft, &a, a.len)});
+    std.debug.print("Median: {}\n", .{median(mft, &b, b.len)});
 }
