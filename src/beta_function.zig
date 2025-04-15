765379e421e93c58448ed08c29743dc19df34ff4
Luca <drwolf85@gmail.com>
2025-04-15 18:34:40 -0400

    Generalized data types for factorial and beta functions

--- src/beta_function.zig
+++ src/beta_function.zig
@@ -2,20 +2,20 @@ const std = @import("std");
 const math = std.math;
 const testing = std.testing;
 
-
-pub export fn lbeta(a: f64, b: f64) f64 {
-    var res : f64 = math.lgamma(f64, a);
-    res += math.lgamma(f64, b);
-    res -= math.lgamma(f64, a + b);
+pub fn lbeta(comptime T: type, a: T, b: T) T {
+    var res: T = @as(T, @floatCast(math.lgamma(f64, @as(f64, @floatCast(a)))));
+    res += @as(T, @floatCast(math.lgamma(f64, @as(f64, @floatCast(b)))));
+    res -= @as(T, @floatCast(math.lgamma(f64, @as(f64, @floatCast(a + b)))));
     return res;
 }
 
-pub export fn beta(a: f64, b: f64) f64 {
-    const res = lbeta(a, b);
+pub fn beta(comptime T: type, a: T, b: T) T {
+    const res = lbeta(T, a, b);
     return @exp(res);
 }
 
 test "Beta-related functions" {
-    std.debug.print("lbeta(5, 2) = {}\n", .{lbeta(5.0,2.0)});
-    std.debug.print("beta(5, 2) = {}\n", .{beta(5.0,2.0)});
+    const mft: type = f64;
+    std.debug.print("lbeta(5, 2) = {}\n", .{lbeta(mft, 5.0, 2.0)});
+    std.debug.print("beta(5, 2) = {}\n", .{beta(mft, 5.0, 2.0)});
 }
--- src/factorial.zig
+++ src/factorial.zig
@@ -2,26 +2,28 @@ const std = @import("std");
 const math = std.math;
 const testing = std.testing;
 
-pub export fn factorial(x: f64) f64 {
-	var res : f64 = x + 1.0;
-	res = math.lgamma(f64, res);
-	return @exp(res);
+pub fn factorial(comptime T: type, x: T) T {
+    var r: f64 = @as(f64, @floatCast(x)) + 1.0;
+    r = math.lgamma(f64, r);
+    const res: T = @as(T, @floatCast(r));
+    return @exp(res);
 }
 
-pub export fn stirling(x: f64) f64 {
-	var res = x * @log(x) - x;
-	res += 0.5 * @log(math.pi * 2.0 * x);
-	return @exp(res);
+pub fn stirling(comptime T: type, x: T) T {
+    var res = x * @log(x) - x;
+    res += 0.5 * @log(math.pi * 2.0 * x);
+    return @exp(res);
 }
 
-pub export fn gosper(x: f64) f64 {
-	var res = x * @log(x) - x;
-	res += 0.5 * @log(math.pi * (2.0 * x + 1.0 / 3.0));
-	return @exp(res);
+pub fn gosper(comptime T: type, x: T) T {
+    var res = x * @log(x) - x;
+    res += 0.5 * @log(math.pi * (2.0 * x + 1.0 / 3.0));
+    return @exp(res);
 }
 
 test "Factorial-related functions" {
-	std.debug.print("Factorial(0.5) = {}\n", .{factorial(0.5)});
-	std.debug.print("Stirling approx = {}\n", .{stirling(0.5)});
-	std.debug.print("Gosper approx = {}\n", .{gosper(0.5)});
+    const mft: type = f64;
+    std.debug.print("Factorial(0.5) = {}\n", .{factorial(mft, 0.5)});
+    std.debug.print("Stirling approx = {}\n", .{stirling(mft, 0.5)});
+    std.debug.print("Gosper approx = {}\n", .{gosper(mft, 0.5)});
 }
