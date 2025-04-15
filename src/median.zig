753c82c39c116c86905ae20a5d48a37f97df3cc1
Luca Sartore <drwolf85@users.noreply.github.com>
2025-04-14 22:08:33 -0400

    Update `median.zig`

--- src/median.zig
+++ src/median.zig
@@ -1,13 +1,22 @@
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn median(len:usize, x: [*]const f64) f64 {
-  @setFloatMode(.Optimized);
-  var res: f64 = 0.0;
-  return res;
+pub export fn median(a: [*]f64, len: usize) f64 {
+        var res : f64 = undefined;
+        const x = a[0..len];
+        const pos : usize = len >> 1;
+        std.mem.sort(f64, x, {}, std.sort.asc(f64));
+        res = x[pos];
+        if (len > 1 and (len & 1) == 0) {
+                res += x[pos-1];
+                res *= 0.5;
+        }
+        return res;
 }
 
-test "basic median functionality" {
-  const a = [_] f64 {0.1, -0.2, -0.5, 1.2, 1.5};
-  std.debug.print("\nMedian {}\n", .{median(a.len, &a)});
+test "Median function" {
+        var a = [_]f64 {0.1, -0.1, -0.2, 1.5, 1.7};
+        var b = [_]f64 {-0.1, -0.2, 1.5, 1.7};
+        std.debug.print("Median: {}\n", .{median(&a, a.len)});
+        std.debug.print("Median: {}\n", .{median(&b, b.len)});
 }
