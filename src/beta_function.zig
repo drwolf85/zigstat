7b8edd16987319c4a96beead3abc6349383e57d4
Luca Sartore <drwolf85@users.noreply.github.com>
2025-04-14 22:05:28 -0400

    Add code related to beta and factorial functions

---
+++ src/beta_function.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const math = std.math;
+const testing = std.testing;
+
+
+pub fn lbeta(a: f64, b: f64) f64 {
+    var res : f64 = math.lgamma(f64, a);
+    res += math.lgamma(f64, b);
+    res -= math.lgamma(f64, a + b);
+    return res;
+}
+
+pub fn beta(a: f64, b: f64) f64 {
+    const res = lbeta(a, b);
+    return @exp(res);
+}
+
+test "Beta-related functions" {
+    std.debug.print("lbeta(5, 2) = {}\n", .{lbeta(5.0,2.0)});
+    std.debug.print("beta(5, 2) = {}\n", .{beta(5.0,2.0)});
+}
---
+++ src/factorial.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const math = std.math;
+const testing = std.testing;
+
+pub fn factorial(x: f64) f64 {
+	var res : f64 = x + 1.0;
+	res = math.lgamma(f64, res);
+	return @exp(res);
+}
+
+pub fn stirling(x: f64) f64 {
+	var res = x * @log(x) - x;
+	res += 0.5 * @log(math.pi * 2.0 * x);
+	return @exp(res);
+}
+
+pub fn gosper(x: f64) f64 {
+	var res = x * @log(x) - x;
+	res += 0.5 * @log(math.pi * (2.0 * x + 1.0 / 3.0));
+	return @exp(res);
+}
+
+test "Factorial-related functions" {
+	std.debug.print("Factorial(0.5) = {}\n", .{factorial(0.5)});
+	std.debug.print("Stirling approx = {}\n", .{stirling(0.5)});
+	std.debug.print("Gosper approx = {}\n", .{gosper(0.5)});
+}
