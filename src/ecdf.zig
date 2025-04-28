f96f1558d61a11a257c13e6915b0b599123a6256
Luca Sartore <drwolf85@gmail.com>
2025-04-28 07:11:54 -0400

    Add univariate ECDF
    Included the code for estimating univariate ECDF

--- src/ecdf.zig
+++ src/ecdf.zig
@@ -15,17 +15,31 @@ fn bin_search(comptime T: type, v: T, a: []T, len: usize) usize {
     return l;
 }
 
-pub fn smooth_ecdf_adimari(comptime T: type, v: T, a: [*]T, len: usize) T {
+pub fn ecdf(comptime T: type, v: T, a: [*]T, len: usize) T {
     var res: T = undefined;
+    if (len == 0) return res;
     var pos: usize = undefined;
     const x = a[0..len];
     std.mem.sort(T, x, {}, std.sort.asc(T));
-    if (len == 0) return res;
     if (v < x[0]) return 0.0;
     if (v >= x[len - 1]) return 1.0;
     const invn: T = 1.0 / @as(T, @floatFromInt(len));
     pos = bin_search(T, v, x, len);
-    res = (@as(T, @floatFromInt(pos + 1)) - 0.5) * invn;
+    res = (@as(T, @floatFromInt(pos)) + 0.5) * invn;
+    return res;
+}
+
+pub fn smooth_ecdf_adimari(comptime T: type, v: T, a: [*]T, len: usize) T {
+    var res: T = undefined;
+    if (len == 0) return res;
+    var pos: usize = undefined;
+    const x = a[0..len];
+    std.mem.sort(T, x, {}, std.sort.asc(T));
+    if (v < x[0]) return 0.0;
+    if (v > x[len - 1]) return 1.0;
+    const invn: T = 1.0 / @as(T, @floatFromInt(len));
+    pos = bin_search(T, v, x, len);
+    res = (@as(T, @floatFromInt(pos)) + 0.5) * invn;
     if (v == x[pos]) return res;
     const lambda: T = (v - x[pos]) / (x[pos + 1] - x[pos]);
     res += lambda * invn;
@@ -35,18 +49,30 @@ pub fn smooth_ecdf_adimari(comptime T: type, v: T, a: [*]T, len: usize) T {
 test "Binary search" {
      const mft: type = f64;
      var a = [_]mft{ 0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8};
-     std.debug.print("Position: {}\n", .{bin_search(mft, 0.144, &a, a.len)});
+     std.debug.print("Position: {}\n\n", .{bin_search(mft, 0.144, &a, a.len)});
+}
+
+test "Empirical cumulative distribution function" {
+    const mft: type = f64;
+    var a = [_]mft{ 0.1, -0.1, -0.2, 1.5, 1.7 };
+
+    std.debug.print("ECDF: {}\n", .{ecdf(mft, -0.1999, &a, a.len)});
+    std.debug.print("ECDF: {}\n", .{ecdf(mft, 0.0001, &a, a.len)});
+    std.debug.print("ECDF: {}\n", .{ecdf(mft, 0.3, &a, a.len)});
+    std.debug.print("ECDF: {}\n", .{ecdf(mft, 0.5, &a, a.len)});
+    std.debug.print("ECDF: {}\n", .{ecdf(mft, 1.6999999999998, &a, a.len)});
+    std.debug.print("ECDF: {}\n\n", .{ecdf(mft, 1.7, &a, a.len)});
 }
 
 test "Smooth empirical cumulative distribution function" {
     const mft: type = f64;
     var a = [_]mft{ 0.1, -0.1, -0.2, 1.5, 1.7 };
 
-    std.debug.print("ECDF: {}\n", .{smooth_ecdf_adimari(mft, -0.1999, &a, a.len)});
-    std.debug.print("ECDF: {}\n", .{smooth_ecdf_adimari(mft, 0.0001, &a, a.len)});
-    std.debug.print("ECDF: {}\n", .{smooth_ecdf_adimari(mft, 0.3, &a, a.len)});
-    std.debug.print("ECDF: {}\n", .{smooth_ecdf_adimari(mft, 0.5, &a, a.len)});
-    std.debug.print("ECDF: {}\n", .{smooth_ecdf_adimari(mft, 1.6999999999998, &a, a.len)});
-    std.debug.print("ECDF: {}\n", .{smooth_ecdf_adimari(mft, 1.7, &a, a.len)});
+    std.debug.print("Smooth ECDF: {}\n", .{smooth_ecdf_adimari(mft, -0.1999, &a, a.len)});
+    std.debug.print("Smooth ECDF: {}\n", .{smooth_ecdf_adimari(mft, 0.0001, &a, a.len)});
+    std.debug.print("Smooth ECDF: {}\n", .{smooth_ecdf_adimari(mft, 0.3, &a, a.len)});
+    std.debug.print("Smooth ECDF: {}\n", .{smooth_ecdf_adimari(mft, 0.5, &a, a.len)});
+    std.debug.print("Smooth ECDF: {}\n", .{smooth_ecdf_adimari(mft, 1.6999999999998, &a, a.len)});
+    std.debug.print("Smooth ECDF: {}\n\n", .{smooth_ecdf_adimari(mft, 1.7, &a, a.len)});
 }
 
